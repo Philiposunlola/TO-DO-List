70771419184c1ac0bb8d7ec4b1cfe7e81700d0f6
Philip <philiposun@gmail.com>
2023-09-18 01:16:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const taskContainer = document.querySelector("#tasks");
+const error = document.querySelector("error");
+const countValue = document.querySelector(".count-value");
+let taskCount = 0;
+
+const dispalyCount = (taskCount) => {
+    countValue.innerText = taskCount;
+};
